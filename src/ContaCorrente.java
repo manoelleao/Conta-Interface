e6369e4f0ce515dc412082e4fa58f2345ed7f849
unknown <manoel.leao@MVBRFORDSK7690.MVFOR.local>
2016-08-10 17:42:07 -0300

    commit

---
+++ src/ContaCorrente.java
@@ -0,0 +1,27 @@
+
+public class ContaCorrente implements Conta, Tributavel{
+
+	@Override
+	public double getSaldo() {
+		return 0;
+	}
+
+	@Override
+	public void deposita(double valor) {
+		
+	}
+
+	@Override
+	public void saca(double valor) {
+		
+	}
+
+	@Override
+	public void atualiza(double taxaSelic) {
+		
+	}
+
+	
+}
+
+
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,24 @@
+
+public class ContaPoupanca implements Conta{
+
+	@Override
+	public double getSaldo() {
+		return 0;
+	}
+
+	@Override
+	public void deposita(double valor) {
+		
+	}
+
+	@Override
+	public void saca(double valor) {
+		
+	}
+
+	@Override
+	public void atualiza(double taxaSelic) {
+		
+	}
+
+}
